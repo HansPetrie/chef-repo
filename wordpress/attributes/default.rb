705c783ac4bc98c0202bb2957f1ce09eec2eaca9
hpetrie <hans.petrie@lookout.com>
2015-10-08 12:56:26 -0700

    Added support for setting the site url to work with changing hostnames

--- wordpress/attributes/default.rb
+++ wordpress/attributes/default.rb
@@ -1,3 +1,4 @@
 default['wordpress']['db_password'] = 'password'
 default['wordpress']['db_user'] = 'root'
 default['wordpress']['db_hostname'] = 'localhost'
+default['wordpress']['site_url'] = 'localhost'
